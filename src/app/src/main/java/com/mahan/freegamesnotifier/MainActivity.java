95ca7d3972eefde964bd5648a74975e976eb5f67
Mahan Noorbahr <m2.noorbahr@gmail.com>
2020-12-06 23:06:05 +0800

    Efficiency
    Changed the code so that it stores the info for free games that were pulled in the previous session. Basically if the list of free games pulled includes a game that was pulled before, we get that saved data so we don't have to make API calls again. This greatly improves the speed and efficiency of the program.

--- src/app/src/main/java/com/mahan/freegamesnotifier/MainActivity.java
+++ src/app/src/main/java/com/mahan/freegamesnotifier/MainActivity.java
@@ -7,13 +7,11 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -30,9 +28,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskInterface {
+public class MainActivity extends AppCompatActivity {
     LinearLayout scrollView;
     TextView placeHolder;
     SwipeRefreshLayout refreshLayout;
@@ -45,6 +47,11 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
     boolean placeHolderShowing;
     RequestQueue rQueue;
+    int postCount,viewCount;
+
+    ArrayList<String[]> gamesList;
+    SharedPreferences sharedPreferences;
+
 
 
     @Override
@@ -64,98 +71,76 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                 refreshLayout.setRefreshing(false);
             }
         });
-        loadGames();
 
+        postCount = 0;
+        gamesList = new ArrayList<>();
+        sharedPreferences = getSharedPreferences("com.mahan.freegamesnotifier",MODE_PRIVATE);
+
+        loadGames();
     }
 
     public void loadGames(){
         placeHolder.setText("Fetching Games.");
         placeHolderShowing = true;
 
-        String url = "https://www.reddit.com/r/freegames/top/.json?t=week";
+        try {
+            String serialized = sharedPreferences.getString("oldPosts",ObjectSerializer.serialize(new ArrayList<String[]>()));
+            final ArrayList<String[]> oldPosts = (ArrayList<String[]>) ObjectSerializer.deserialize(serialized);
+            String url = "https://www.reddit.com/r/freegames/top/.json?t=week";
 
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            setJson(new JSONObject(response));
-                        } catch (JSONException e) {
-                            placeHolder.setText("Failed to get games.");
+            StringRequest request = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                setJson(oldPosts,new JSONObject(response));
+                            } catch (JSONException e) {
+                                placeHolder.setText("Failed to get games.");
+                            }
                         }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                placeHolder.setText("Failed to get games.");
-            }
-        });
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    placeHolder.setText("Failed to get games.");
+                }
+            });
 
-        rQueue.add(request);
-    }
-
-
-
-    public void setImage(final String image, final String image2, final String name, final String storeLink, final String gameDescription){
-        /*
-        This method is called by JsonTaskRawG which provides the information for a given game.
-         */
-
-        //Removes the placeholder
-        if(placeHolderShowing){
-            scrollView.removeAllViews();
-            placeHolderShowing = false;
+            rQueue.add(request);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        //Inflates a pre-made postLayout and sets the values for image view and text view.
-        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        ConstraintLayout postLayout =  (ConstraintLayout) inflater.inflate(R.layout.post_layout,null);
-
-        SelectableRoundedImageView imgView = postLayout.findViewById(R.id.imageView);
-        final TextView textView = postLayout.findViewById(R.id.postName);
-
-        //Using the Glide library, load the image. Set game name.
-        Glide.with(this).load(image).into(imgView);
-        textView.setText(name);
-
-        final MainActivity context = this;
-
-        //Start a new intent when the game layout is clicked
-        postLayout.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onClick(View v) {
-                NotificationHelper n = new NotificationHelper(context);
-                n.getNotification("MyTitle","MyString");
-                Intent intent = new Intent(context,gameDetail.class);
-                intent.putExtra(EXTRA_IMAGE1, image);
-                intent.putExtra(EXTRA_IMAGE2,image2);
-                intent.putExtra(EXTRA_TITLE,name);
-                intent.putExtra(EXTRA_LINK,storeLink);
-                intent.putExtra(EXTRA_DESC,gameDescription);
-                startActivity(intent);
-
-            }
-        });
-
-        //Add the game to the view.
-        scrollView.addView(postLayout);
     }
 
-    public void setJson(JSONObject json) {
+    public void setJson(final ArrayList<String[]> savedPosts, final JSONObject json) {
         /*
-        Method implemented for the JsonTask Class to use.
-        Handles the json file returned by JsonTask.
+        Handles the json file returned by Volley request.
         In this case it goes through post inside "children" array and turns it into a game.
          */
 
-        try {
-            JSONArray posts = json.getJSONObject("data").getJSONArray("children");
 
-            for(int i = 0; i < posts.length(); i++){
-                JSONObject post = posts.getJSONObject(i).getJSONObject("data");
-                String title = post.getString("title");
+        try {
+            final JSONArray posts = json.getJSONObject("data").getJSONArray("children");
+            List<Object> list = separatePosts(savedPosts,posts);
+
+            ArrayList<String[]> oldPosts = (ArrayList<String[]>) list.get(0);
+            JSONArray newPosts = (JSONArray) list.get(1);
+
+            postCount = oldPosts.size() + newPosts.length();
+
+            //FOR OLD POSTS
+            for (int i = 0; i < oldPosts.size(); i++) {
+                String[] post = oldPosts.get(i);
+                System.out.println("OLD POST: " + post[0]);
+                setImage(post[0],post[1],post[2],post[3],post[4],post[5]);
+            }
+
+            //FOR NEW POSTS
+            for(int i = 0; i < newPosts.length(); i++){
+                JSONObject post = newPosts.getJSONObject(i).getJSONObject("data");
+                final String title = post.getString("title");
                 final String link = post.getString("url");
+
+                System.out.println("NEW POST: " + title);
 
                 String url = "https://api.rawg.io/api/games?search="+ title.replace(" ","+");
 
@@ -197,7 +182,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                                                         }
                                                         else{imgURL2 = "NULL";}
 
-                                                        setImage(imgURL,imgURL2,name,link,gameDescription);
+                                                        setImage(title,name,imgURL,imgURL2,gameDescription,link);
                                                     } catch (JSONException e) {
                                                         e.printStackTrace();
                                                     }
@@ -211,6 +196,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                                     });
 
                                     rQueue.add(request2);
+
 
                                 } catch (JSONException e) {
                                     e.printStackTrace();
@@ -226,13 +212,96 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                 });
 
                 rQueue.add(request);
-
             }
+
 
         } catch (JSONException e) {
             placeHolder.setText("Failed to get games.");
         }
 
+
     }
 
+    public void setImage(final String redditName, final String name, final String image, final String image2, final String gameDescription, final String storeLink){
+
+        gamesList.add(new String[]{redditName,name,image,image2,gameDescription,storeLink});
+        viewCount += 1;
+
+        //Removes the placeholder
+        if(placeHolderShowing){
+            scrollView.removeAllViews();
+            placeHolderShowing = false;
+        }
+
+        //Inflates a pre-made postLayout and sets the values for image view and text view.
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ConstraintLayout postLayout =  (ConstraintLayout) inflater.inflate(R.layout.post_layout,null);
+
+        SelectableRoundedImageView imgView = postLayout.findViewById(R.id.imageView);
+        final TextView textView = postLayout.findViewById(R.id.postName);
+
+        //Using the Glide library, load the image. Set game name.
+        Glide.with(this).load(image).into(imgView);
+        textView.setText(name);
+
+        final MainActivity context = this;
+
+        //Start a new intent when the game layout is clicked
+        postLayout.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                NotificationHelper n = new NotificationHelper(context);
+                n.getNotification("MyTitle","MyString");
+                Intent intent = new Intent(context,gameDetail.class);
+                intent.putExtra(EXTRA_IMAGE1, image);
+                intent.putExtra(EXTRA_IMAGE2,image2);
+                intent.putExtra(EXTRA_TITLE,name);
+                intent.putExtra(EXTRA_LINK,storeLink);
+                intent.putExtra(EXTRA_DESC,gameDescription);
+                startActivity(intent);
+
+            }
+        });
+
+        //Add the game to the view.
+        scrollView.addView(postLayout);
+
+        if(viewCount==postCount){storeGames();}
+
+    }
+
+    private void storeGames() {
+        try {
+            String serialized = ObjectSerializer.serialize(gamesList);
+            sharedPreferences.edit().putString("oldPosts",serialized).apply();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private List<Object> separatePosts(ArrayList<String[]> oldPosts,JSONArray newPosts){
+        ArrayList<String[]> oldPostsInNew = new ArrayList<>();
+
+        try {
+
+            for(int i=0;i<newPosts.length();i++){
+                JSONObject post =  newPosts.getJSONObject(i);
+                String postTitle = post.getJSONObject("data").getString("title");
+                for(int j=0; j<oldPosts.size();j++){
+                    String title = oldPosts.get(j)[0];
+                    if(title.equals(postTitle)){
+                        oldPostsInNew.add(oldPosts.get(j));
+                        newPosts.remove(i);
+                        i--;
+                        break;
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return Arrays.asList(oldPostsInNew,newPosts);
+    }
 }
